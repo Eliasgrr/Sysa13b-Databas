5bb684699a5dd620b4f9cb4f8ee34200eb101932
alexkvarnmark <44166622+alexkvarnmark@users.noreply.github.com>
2019-09-20 12:35:14 +0200

    main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,36 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.ResultSet;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try { 
+			
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+			
+			String url = "jdbc:sqlserver://localhost:1433;database=sysb13";
+			String user = "user1";
+			String password = "data";
+			
+			Connection myConnection = DriverManager.getConnection(url, user, password);
+			
+			String myQuery = "SELECT * FROM Student";
+			
+			PreparedStatement myPreparedStatement = myConnection.prepareStatement(myQuery);
+			
+			ResultSet myResultSet = myPreparedStatement.executeQuery();
+			
+		}
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			}
+		}
 
 	}
 
-}
+
